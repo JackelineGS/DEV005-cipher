85abcfdafc344eebb78ad8107be5d0a1d936de9e
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-02-27 20:21:18 -0500

    Version lunes 27

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,35 @@
+/*eslint-disable eqeqeq*/
+
 const cipher = {
-  // ...
-};
+  encode: function(offset, mensaje) {
+    if (!offset || !mensaje) {
+      throw new TypeError("Whoops!");
+    }
+    let resultado1 = "";
+    for (let i = 0; i < mensaje.length; i++) {
+      resultado1 = resultado1 + String.fromCharCode((mensaje.charCodeAt(i) + offset - 65)%26 + 65); 
+    }
+    return resultado1;
+  },
+  decode: function(offset, mensaje) {
+    if (!offset || !mensaje) {
+      throw new TypeError("Whoops!");
+    }
+    let resultado2 = "";
+    for (let i = 0; i < mensaje.length; i++) {
+      resultado2 = resultado2 + String.fromCharCode((mensaje.charCodeAt(i) - offset + 65)%26 + 65);   
+      
+    }
+    return resultado2;
+  } 
+
+}
 
 export default cipher;
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
+/*eslint-disable no-unused-vars*/
+/*eslint-disable eqeqeq*/ 
+/*eslint-disable no-undef*/ 
+/*eslint-disable no-console*/
+/*eslint-disable indent*/
+
 import cipher from './cipher.js';
 
-console.log(cipher);
+const cifrarMensaje = document.getElementById('boton-Cifrar');
+const descifrarMensaje = document.getElementById('boton-Descifrar');
+
+//codificar mensaje
+
+cifrarMensaje.addEventListener("click", function() {
+  const mensajeEntrada = document.getElementById('message').value
+  const offset = document.getElementById('offset').value
+  const mensajeCodificado = document.getElementById('resultado');
+  mensajeCodificado.value = cipher.encode(parseInt(offset), mensajeEntrada);
+  }
+) 
+
+//Decodificar Mensaje
+
+descifrarMensaje.addEventListener("click", function() {
+  const offset = document.getElementById('offset').value
+  const mensajeEntrada = document.getElementById('message').value
+  const mensajeCodificado = document.getElementById('resultado');
+  mensajeCodificado.value = cipher.decode(parseInt(offset),mensajeEntrada);
+  
+  }
+) 
+
+//funcion reiniciar
+const reiniciar = document.getElementById('reiniciar');
+reiniciar.addEventListener('click', function() {
+  document.getElementById('formulario').reset();
+}
+)
+
+const formulario = document.querySelector("#formulario");
+formulario.addEventListener("submit", function(event){
+  console.log(event);
+  event.preventDefault();
+}) 
+
+
+
+
+
+
