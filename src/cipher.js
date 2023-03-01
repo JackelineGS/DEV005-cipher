44c062158cfa2685eb378df0ca2ddaf0cdb7fcba
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-02-28 20:04:20 -0500

    version martes 28

--- src/cipher.js
+++ src/cipher.js
@@ -7,7 +7,12 @@ const cipher = {
     }
     let resultado1 = "";
     for (let i = 0; i < mensaje.length; i++) {
-      resultado1 = resultado1 + String.fromCharCode((mensaje.charCodeAt(i) + offset - 65)%26 + 65); 
+      if (mensaje.charCodeAt(i) >= 32 && mensaje.charCodeAt(i) <= 64) {
+        resultado1 += mensaje[i];
+      }
+      else {
+        resultado1 = resultado1 + String.fromCharCode((mensaje.charCodeAt(i) + offset - 65)%26 + 65); 
+      }
     }
     return resultado1;
   },
@@ -17,8 +22,12 @@ const cipher = {
     }
     let resultado2 = "";
     for (let i = 0; i < mensaje.length; i++) {
-      resultado2 = resultado2 + String.fromCharCode((mensaje.charCodeAt(i) - offset + 65)%26 + 65);   
-      
+      if(mensaje.charCodeAt(i) >= 32 && mensaje.charCodeAt(i) <= 64) {
+        resultado2 += mensaje[i];
+      }
+      else{
+        resultado2 = resultado2 + String.fromCharCode((mensaje.charCodeAt(i) - offset + 65)%26 + 65);
+      }   
     }
     return resultado2;
   } 
